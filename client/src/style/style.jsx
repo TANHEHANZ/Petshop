c217d6ed2020c28e072836b04e314d51f610da99
gonzaa89 <cbbe.alangonzalo.silvestre.qu@unifranz.edu.bo>
2023-10-23 19:53:33 -0400

    stylelogin

--- client/src/style/style.jsx
+++ client/src/style/style.jsx
@@ -4,15 +4,79 @@ export const Content = styled.div`
   width: 100vw;
   height: 100vh;
   ${FlexComun}
+  background-color:${colors.primary};
   & form {
-    ${FlexComun}
-    flex-direction:column;
-    width: 40vw;
-    height: 30em;
+    display: flex;
+    align-items: start;
+    justify-content: start;
+    flex-direction: column;
+    width: 30em;
+    height: 33em;
     background-color: ${colors.secundary};
-    color: ${colors.primary};
+    color: ${colors.white};
+    padding: 2em;
+    gap: 0.5em;
+    border-radius: 1em;
+    box-shadow: 0 10px 20px ${colors.secundary};
+
+    & h1 {
+      width: 100%;
+      text-align: center;
+      font-size: 2em;
+      margin: 1em 0;
+      font-weight: 600;
+    }
+    & label {
+      font-size: 0.8em;
+      width: 100%;
+      text-align: start;
+      color: ${colors.tercery};
+    }
+    & input {
+      width: 100%;
+      background-color: transparent;
+      border: none;
+      border-bottom: solid 1px ${colors.white};
+      padding: 1em;
+      outline: none;
+      color:${colors.tercery};
+    }
+    & > input:focus {
+      border-bottom: solid 1px ${colors.tercery};
+    }
+    & > button {
+      margin: 2em 0;
+      text-transform: uppercase;
+      background-color: ${colors.secundary};
+      border: none;
+      position: relative;
+      color: ${colors.tercery};
+      cursor: pointer;
+      overflow: hidden;
+      height: 3em;
+      width: 10em;
+      &::before {
+        content: "";
+        position: absolute;
+        background-color: ${colors.tercery};
+        width: 120%;
+        height: 120%;
+        z-index: -1;
+        animation: rotate 2s infinite;
+      }
+    }
+    @keyframes rotate {
+      0% {
+        transform: rotate(0);
+      }
+      100% {
+        transform: rotate(360deg);
+      }
+    }
   }
   & div {
     width: 50vw;
+    height: 90vh;
+    background-color: #000;
   }
 `;
